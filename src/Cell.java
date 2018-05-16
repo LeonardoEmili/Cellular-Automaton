564dd18d249b474e2e884dff92fc4fc5cd44aae8
leoemili <leochicco1998@gmail.com>
2018-05-16 09:35:32 +0200

    Demo commit

---
+++ src/Cell.java
@@ -0,0 +1,9 @@
+public class Cell {
+
+    int y, x;
+
+    public Cell(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+}
---
+++ src/NonSyncBoard.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+public class NonSyncBoard {
+
+    private int h, w;
+    private Cell[][] grid;
+    private int population;
+    private Random generator;
+
+    public NonSyncBoard(int h, int w) {
+        this.h = h;
+        this.w = w;
+        this.population = h * w * 100 / 30;
+        this.grid = new Cell[h][w];
+        this.generator = new Random();
+        setPopulation();
+    }
+
+    public void setPopulation() {
+        for (int i = 0; i < this.population; i++) {
+            int newY = generator.nextInt(this.h);
+            int newX = generator.nextInt(this.w);
+            while (this.grid[newY][newX] != null) {
+                newY = generator.nextInt(this.h);
+                newX = generator.nextInt(this.w);
+            }
+            grid[newY][newX] = new Cell(newY, newX);
+        }
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public int getW() {
+        return w;
+    }
+}
